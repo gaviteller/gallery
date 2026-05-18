425dc0b270d1e2b43e59bb3d9079c5160dcda794
Gavriel Teller <gavrielteller@gmail.com>
2026-05-18 18:40:44 -0400

    fix: cap feed image height at 45vh so it fits on screen

--- app/page.tsx
+++ app/page.tsx
@@ -206,7 +206,7 @@ export default function FeedPage() {
                   <img
                     src={post.image}
                     alt={post.description ?? ""}
-                    style={{ borderRadius: 12, width: "100%", height: 260, objectFit: "cover", display: "block" }}
+                    style={{ borderRadius: 12, width: "100%", maxHeight: "45vh", objectFit: "cover", display: "block" }}
                   />
                 </button>
 
